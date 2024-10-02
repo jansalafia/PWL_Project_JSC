a571eb218ee014b80cd9c03ae6fa1e20696b9157
jansalafia <144460301+jansalafia@users.noreply.github.com>
2024-10-02 09:56:21 -0400

    Co-authored-by: Carlos-castill0 <Carlos-castill0@users.noreply.github.com> Co-authored-by: SeanSalafia <SeanSalafia@users.noreply.github.com>

--- src/Article.java
+++ src/Article.java
@@ -3,14 +3,14 @@ import java.nio.file.*;
 
 public class Article {
 
-public final String[] ArticleArray;
+public String Article;
     public String userInputArticle;
 
     public Article(String data) {
     this.userInputArticle = data;
     }
 
-    public String[] assignArticle(String article){
+    public String []assignArticle(String article){
         String[] splitArticle = article.split(article);
         System.out.println(splitArticle);
         return splitArticle;
@@ -33,6 +33,7 @@ public final String[] ArticleArray;
     {
         //Read and Print the Center File
         String centerArticleString = readFileAsString("News Articles\\Center.txt");
+       
         //System.out.println(centerArticleString);
 
         System.out.println("******************************************************************");
@@ -40,13 +41,35 @@ public final String[] ArticleArray;
         //Read and Print the Left File
         String leftArticleString = readFileAsString("News Articles\\Left.txt");
         //System.out.println(leftArticleString);
-
+        
         System.out.println("******************************************************************");
 
         //Read and Print the Right File
         String rightArticleString = readFileAsString("News Articles\\Right.txt");
         //System.out.println(rightArticleString);
 
-        Article FirstArticle = new Article();
+
+        Article FirstArticle = new Article(centerArticleString);
+        String FirstArticleApostrophe = FirstArticle.userInputArticle.replaceAll("[\'’]", "");
+
+        String FirstArticleLowercase = FirstArticleApostrophe.replaceAll("[^a-zA-Z]", " ");
+        String FirstArticleClean = FirstArticleLowercase.replaceAll("( )+", " ");
+
+        //Print clean article
+        System.out.println(FirstArticleClean);
+
+
+        //Article SecondArtcle = new Article(leftArticleString);
+        //String SecondArticleLowercase = SecondArtcle.userInputArticle.replaceAll("'", " ");
+        //SecondArticleLowercase = SecondArticleA
+
+        // Article SecondArticlCleanleftArticleString);
+        // String SecondArticleStopWordsRemoved = SecondArticle.userInputArticle.replaceAll("[^a-zA-Z]", "");
+        // String SecondArticleRemovedSpaces = SecondArticle.userInputArticle.replaceAll("[^   ]", " ");
+
+        //Article ThirdArticlCleanrightArticleString);
+        ///String ThirdArticleStopWordsRemoved = ThirdArticle.userInputArticle.replaceAll("[^a-zA-Z]", "");
+        ///String ThirdArticleRemovedSpaces = FirstArticle.userInputArticle.replaceAll("[^   ]", " ");
+        
     }
-}
+}   
